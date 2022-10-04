c2d72e015981ef90dda58616a9553b1fc0000263
aidanbabo <40786070+aidanbabo@users.noreply.github.com>
2022-10-04 16:25:11 -0400

    added the dumb main
    so dumb :facepunch: :frowning:

--- main.py
+++ main.py
@@ -2,8 +2,13 @@ import flask
 
 app = flask.Flask(__name__)
 
+
 @app.route('/')
 @app.route('/index.html')
 @app.route('/index')
 def hello_world():
     return flask.Response("Hello World", mimetype='text/html')
+
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8080, debug=True)
