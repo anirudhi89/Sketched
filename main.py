c581fd341156b592ba3cf0609bac116ad6bb5c69
anirudhi89 <anirudh.iyer@outlook.com>
2022-11-09 20:02:25 -0500

    FINALLY!

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 import flask
 from flask import request
 import json
+import random
+from google.cloud import datastore
 
 app = flask.Flask(__name__)
 
-
+client = datastore.Client(project='sketched')
 
 
 @app.route('/')
 @app.route('/index.html')
 @app.route('/index')
 def root():
+    # verify_auth()
     return flask.render_template("index.html", page_title='Sketched | Home')
 
 @app.route('/upload/confirm', methods = ["POST"])
@@ -26,16 +29,39 @@ def submit_confirm():
     request_json = request.get_json(force=True)
     url = request_json.get("url")
     if url != None:
-        return "success"
+        kind = "StickyNote"
+        name = "sticky"+ random.randint(0, 200)
+        task_key = datastore.client.key(kind,name)
+        task = datastore.Entity(key=task_key)
+        task['url'] = url
+        datastore.put(task)
+        return "Success! Image Uploaded"
     else:
-        return "error"
+        return "Error. Unable to upload image."
     
 @app.route('/submit/redirConfirm<message>', methods=["GET"])
 def submit_confirmation(message):
     return flask.render_template("confirm.html", value = message)
 
+@app.route('get/sketches')
+def get_sketches():
+    request_json = request.get_json(force=True)
+    url = request_json.get("id")
+    
 
 if __name__ == '__main__':
     app.debug = True
     app.run()
     #JUST FOR DEBUGGING
+
+
+
+
+#Helper Methods
+
+def get_from_ds(id):
+    kind = "Sticky Note"
+    name = 'sample stickyNote'
+    user = id
+    client.query(user=id)
+    
