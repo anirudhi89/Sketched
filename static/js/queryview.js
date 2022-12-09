3d465c2d0947082600c0ef3bf9dc9ce9c92adcc5
anirudhi89 <anirudh.iyer@outlook.com>
2022-12-08 20:28:29 -0500

    display images

--- static/js/queryview.js
+++ static/js/queryview.js
@@ -13,18 +13,22 @@
 //     }
 // })
 
-//post a request, get all sketches from us
-async function getIMGs() {
-    return await fetch('/get/images');
-  }
-  getIMGs().then(response => response.text())
-  .then(response => {
-    console.log(response)
-     // => 'Page not found'
-  });
 
-function generate() {
-    var arr = [4]
+window.onload = function() {
+    var arr = []
+    //post a request, get all sketches from us
+
+
+    async function getIMGs() {
+        return await fetch('/get/images');
+    }
+    getIMGs().then(response => response.text())
+    .then(response => {
+        console.log(response)
+        arr = response;
+        // => 'Page not found'
+    });
+
     const tbl = document.createElement("table");
     const tblBody = document.createElement("tbody");
     const location = document.getElementById('test')
@@ -32,27 +36,25 @@ function generate() {
     var rows;
     var cols;
     const imgURL = 'https://i.etsystatic.com/18883082/r/il/24ac06/2116869557/il_570xN.2116869557_s9ue.jpg'
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] > 6) {
-            cols = 3;
-            rows = Math.ceil(arr[i]/3);
-        }
-        else if (arr[i] === 6) {
-            cols = 3;
-            rows = 2;
-        }
-        else if (arr[i] === 4) {
-            rows = 2;
-            cols = 2;
-        }
+    if (arr.length > 6) {
+        cols = 3;
+        rows = Math.ceil(arr.length/3);
     }
-
+    else if (arr.length === 6) {
+        cols = 3;
+        rows = 2;
+    }
+    else if (arr.length === 4) {
+        rows = 2;
+        cols = 2;
+    }       
+        
     for (let j = 0; j < rows; j++) {
         const rowHTML = document.createElement('tr')
         for (let k = 0; k < cols; k++) {
             const td = document.createElement("td");
             const cellImg = document.createElement('img');
-            cellImg.src = imgURL;
+            cellImg.src = arr;
             td.appendChild(cellImg);
             rowHTML.appendChild(td);
         }
@@ -62,5 +64,8 @@ function generate() {
     tbl.appendChild(tblBody);
     location.appendChild(tbl);
     tbl.setAttribute("border", "2");
+    for (let i = 1; i < arr.length + 1; i++) {
+        var imgtemp = document.getElementById(i.toString());
+        imgtemp.src = arr[i-1];
+    }
 }
-
