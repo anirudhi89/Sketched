c027da0e8f1738cf1f79e30e425d26b31fae25df
anirudhi89 <anirudh.iyer@outlook.com>
2022-12-08 22:18:52 -0500

    changes

--- static/js/queryview.js
+++ static/js/queryview.js
@@ -26,47 +26,10 @@ window.onload = async function() {
          .then(response => response.text())
          .then(response => {
             array = response
+            console.log("response")
+            console.log(response)
          })
-         const tbl = document.createElement("table");
-        const tblBody = document.createElement("tbody");
-        const location = document.getElementById('temp')
-
-        var rows;
-        var cols;
-        if (arr.length > 6) {
-            cols = 3;
-            rows = Math.ceil(arr.length/3);
-        }
-        else if (arr.length === 6) {
-            cols = 3;
-            rows = 2;
-        }
-        else if (arr.length === 4) {
-            rows = 2;
-            cols = 2;
-        }
-        var counter = 1;
-        for (let j = 0; j < rows; j++) {
-            const rowHTML = document.createElement('tr')
-            for (let k = 0; k < cols; k++) {
-                const td = document.createElement("td");
-                const cellImg = document.createElement('img');
-                cellImg.id = counter;
-                cellImg.src = ''
-                counter++
-                td.appendChild(cellImg);
-                rowHTML.appendChild(td);
-            }
-            tblBody.appendChild(rowHTML);
-        }
-
-        tbl.appendChild(tblBody);
-        location.appendChild(tbl);
-        tbl.setAttribute("border", "2");
-        for (let i = 1; i < arr.length + 1; i++) {
-            var imgtemp = document.getElementById(i.toString());
-            imgtemp.src = arr[i-1];
-        }
+        return array
     }
     async function getIMGs() {
         await fetch('/get/images', {
@@ -76,47 +39,45 @@ window.onload = async function() {
           .then(response => response.text())
           .then(response => { arr = response; });
     }
-    // arr = await getImages()
-    // console.log(arr)
-    // const tbl = document.createElement("table");
-    // const tblBody = document.createElement("tbody");
-    // const location = document.getElementById('temp')
+    arr = await getImages().then(() => console.log("test"))
+    console.log(arr)
+    const tbl = document.createElement("table");
+    const tblBody = document.createElement("tbody");
+    const location = document.getElementById('temp')
 
-    // var rows;
-    // var cols;
-    // if (arr.length > 6) {
-    //     cols = 3;
-    //     rows = Math.ceil(arr.length/3);
-    // }
-    // else if (arr.length === 6) {
-    //     cols = 3;
-    //     rows = 2;
-    // }
-    // else if (arr.length === 4) {
-    //     rows = 2;
-    //     cols = 2;
-    // }
-    // var counter = 1;
-    // for (let j = 0; j < rows; j++) {
-    //     const rowHTML = document.createElement('tr')
-    //     for (let k = 0; k < cols; k++) {
-    //         const td = document.createElement("td");
-    //         const cellImg = document.createElement('img');
-    //         cellImg.id = counter;
-    //         cellImg.src = ''
-    //         counter++
-    //         td.appendChild(cellImg);
-    //         rowHTML.appendChild(td);
-    //     }
-    //     tblBody.appendChild(rowHTML);
-    // }
+    var rows;
+    var cols;
+    if (arr.length > 6) {
+        cols = 3;
+        rows = Math.ceil(arr.length/3);
+    }
+    else if (arr.length === 6) {
+        cols = 3;
+        rows = 2;
+    }
+    else if (arr.length === 4) {
+        rows = 2;
+        cols = 2;
+    }       
+        
+    for (let j = 0; j < rows; j++) {
+        const rowHTML = document.createElement('tr')
+        for (let k = 0; k < cols; k++) {
+            const td = document.createElement("td");
+            const cellImg = document.createElement('img');
+            cellImg.src = arr;
+            td.appendChild(cellImg);
+            rowHTML.appendChild(td);
+        }
+        tblBody.appendChild(rowHTML);
+    }
 
-    // tbl.appendChild(tblBody);
-    // location.appendChild(tbl);
-    // tbl.setAttribute("border", "2");
-    // for (let i = 1; i < arr.length + 1; i++) {
-    //     var imgtemp = document.getElementById(i.toString());
-    //     imgtemp.src = arr[i-1];
-    // }
+    tbl.appendChild(tblBody);
+    location.appendChild(tbl);
+    tbl.setAttribute("border", "2");
+    for (let i = 1; i < arr.length + 1; i++) {
+        var imgtemp = document.getElementById(i.toString());
+        imgtemp.src = arr[i-1];
+    }
     
 }
