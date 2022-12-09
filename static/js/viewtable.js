e3030a53340490e85b26fc1513da1eae10ca5c5e
Ethan Rubenstein <ethanrubenstein2@gmail.com>
2022-12-08 23:10:22 -0500

    fix for viewtable

--- static/js/viewtable.js
+++ static/js/viewtable.js
@@ -1,5 +1,5 @@
 window.onload = function() {
-    var arr = ["https://storage.googleapis.com/sketched-bucket/helloworld","https://storage.googleapis.com/sketched-bucket/hisketch","https://storage.googleapis.com/sketched-bucket/testsketch","https://storage.googleapis.com/sketched-bucket/samplesketch"]
+    var arr = ["https://storage.googleapis.com/sketched-bucket/helloworld","https://storage.googleapis.com/sketched-bucket/hisketch","https://storage.googleapis.com/sketched-bucket/othersketch","https://storage.googleapis.com/sketched-bucket/samplesketch"]
     const tbl = document.createElement("table");
     const tblBody = document.createElement("tbody");
     const location = document.getElementById('temp')
@@ -21,9 +21,12 @@ window.onload = function() {
     var counter = 1;
     for (let j = 0; j < rows; j++) {
         const rowHTML = document.createElement('tr')
+        rowHTML.style.border = "transparent";
         for (let k = 0; k < cols; k++) {
             const td = document.createElement("td");
             const cellImg = document.createElement('img');
+            cellImg.style.height='50%'; // CHANGE AS NEEDED
+            cellImg.style.width='50%'; // CHANGE AS NEEDED
             cellImg.id = counter;
             cellImg.src = ''
             counter++
@@ -36,6 +39,9 @@ window.onload = function() {
     tbl.appendChild(tblBody);
     location.appendChild(tbl);
     tbl.setAttribute("border", "2");
+    location.setAttribute('height', '500px')
+    location.setAttribute('width', '1500px')
+
     for (let i = 1; i < arr.length + 1; i++) {
         var imgtemp = document.getElementById(i.toString());
         imgtemp.src = arr[i-1];
